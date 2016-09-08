e37b22d5834533a4a066af1485487dcb3b948d1a
gkontzle <gkontzle@STUPID-PC713.stnet.local>
2016-09-08 15:31:52 +0300

    Fixed 3 issues

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 public class TennisGameTest {
 
+	public void NewGame(){
+		TennisGame game= new TennisGame();
+	}
+	
 	@Test
 	public void testTennisGame_Start() {
 		//Arrange
@@ -47,4 +51,74 @@ public class TennisGameTest {
 		// This statement should cause an exception
 		game.player1Scored();			
 	}		
+	@Test
+	public void testTennisGame_deuce_at3points() throws TennisGameException{
+		TennisGame game= new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		String score=game.getScore();
+		assertEquals("Deuce score is incorrect","deuce",score);
+		
+	}
+	
+	@Test
+	public void testTennisGame_Player2_adv() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		
+		String score=game.getScore();
+		assertEquals("Player2 adv is incorrect","player2 has advantage", score);
+		
+		
+		
+	}
+	@Test
+	public void testTennisGame_Player2win() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		
+		String score= game.getScore();
+		assertEquals("Player 2 did not win", "player2 wins", score);
+		
+	}
+	
+	@Test
+	public void testTennisGame_Player2_scores() throws TennisGameException{
+		TennisGame game= new TennisGame();
+		
+		game.player2Scored();
+		String score = game.getScore();
+		
+		assertEquals("Player2 did not scores","love - 15",score);
+		
+	}
+	
+	@Test
+	public void testTennisGame_Player1_scores() throws TennisGameException{
+		TennisGame game= new TennisGame();
+		
+		game.player1Scored();
+		String score = game.getScore();
+		
+		assertEquals("Player1 did not scores","15 - love",score);
+		
+	}
+	
 }
