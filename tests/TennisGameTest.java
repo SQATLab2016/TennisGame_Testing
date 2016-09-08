35f045f5b09e4a64e98eec3ddea0463c09db8442
jon bilbao <astrainjb25@gmail.com>
2016-09-08 15:24:02 +0300

    mistakes corrected

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -14,6 +14,7 @@ public class TennisGameTest {
 		assertEquals("Initial score incorrect", "love - love", score);		
 	}
 	
+	
 	@Test
 	public void testTennisGame_EahcPlayerWin4Points_Score_Deuce() throws TennisGameException {
 		//Arrange
@@ -35,6 +36,98 @@ public class TennisGameTest {
 		assertEquals("Tie score incorrect", "deuce", score);		
 	}
 	
+	
+	/*
+	 * mistake corrected
+	*/
+	@Test
+	public void testTennisGame_scorePoints_40_30() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		
+		//game.player1Scored();
+		game.player2Scored();
+		
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "player2 has advantage", score);		
+	}
+	
+	/*
+	 * mistake corrected
+	*/	
+	@Test
+	public void testTennisGame_scorePoints_30_15() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+	
+		
+		game.player2Scored();
+		
+		
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "30 - 15", score);		
+	}
+	
+	@Test
+	public void testTennisGame_Player1HasAdvantagePoints_Score_Advantage() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "player1 has advantage", score);		
+	}
+	
+	@Test
+	public void testTennisGame_Player2HasAdvantagePoints_Score_Advantage() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player2Scored();
+		
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "player2 has advantage", score);		
+	}
 	@Test (expected = TennisGameException.class)
 	public void testTennisGame_Player1WinsPointAfterGameEnded_ResultsException() throws TennisGameException {
 		//Arrange
@@ -47,4 +140,18 @@ public class TennisGameTest {
 		// This statement should cause an exception
 		game.player1Scored();			
 	}		
+	
+	
+	@Test (expected = TennisGameException.class)
+	public void testTennisGame_Player2WinsPointAfterGameEnded_ResultsException() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		// This statement should cause an exception
+		game.player2Scored();			
+	}	
 }
