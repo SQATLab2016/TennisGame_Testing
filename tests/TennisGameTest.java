329d2f573a49a25c3c1c4812b4a75de6f59c068b
heiheisk <heiheisk@STUPID-PC727.stnet.local>
2016-09-09 15:28:07 +0300

    Assignment finished. Player2 advantage had bug in the logic and public method getScore returns player2score first (not actually a bug, but a weird thing to do).

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -18,6 +18,131 @@ public class TennisGameTest {
 // "player2 has advantage"
 // "player1 wins"
 // "player2 wins"
+	
+	// My tests begin ...
+	@Test
+	public void testTennisGame_player1Win_4Points() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 4; i++) {
+			game.player1Scored();
+		}
+		
+		assertEquals("player1 should have won.", "player1 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_player2Win_4Points() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 4; i++) {
+			game.player2Scored();
+		}
+		
+		assertEquals("player2 should have won.", "player2 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_player1_advantage() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 3; i++) {
+			game.player1Scored();
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			game.player2Scored();
+		}
+		
+		game.player1Scored();
+		
+		assertEquals("player1 should have advantage.", "player1 has advantage", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_player2_advantage() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 3; i++) {
+			game.player2Scored();
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			game.player1Scored();
+		}
+		
+		game.player2Scored();
+		
+		assertEquals("player2 should have advantage.", "player2 has advantage", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_15_15() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		assertEquals("Should be 15 - 15.", "15 - 15", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_30_30() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 2; i++) {
+			game.player1Scored();
+		}
+		
+		for(int i = 0; i < 2; i++) {
+			game.player2Scored();
+		}
+		
+		assertEquals("Should be 30 - 30.", "30 - 30", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_p1_40_p2_15() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 2; i++) {
+			game.player1Scored();
+		}
+		
+		game.player2Scored();
+		game.player1Scored();
+		
+		// getscore returns p2 - p1
+		assertEquals("Should be 15 - 40.", "15 - 40", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_p1_15_p2_40() throws TennisGameException {
+		
+		TennisGame game = new TennisGame();
+		
+		for(int i = 0; i < 2; i++) {
+			game.player2Scored();
+		}
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		// getscore returns p2 - p1
+		assertEquals("Should be 40 - 15.", "40 - 15", game.getScore());
+	}
+	// My tests end.
+	
+	// ---------------------------------------------------------------
+	
+	// examples begin ...
 	@Test
 	public void testTennisGame_Start() {
 		//Arrange
@@ -29,7 +154,7 @@ public class TennisGameTest {
 	}
 	
 	@Test
-	public void testTennisGame_EahcPlayerWin4Points_Score_Deuce() throws TennisGameException {
+	public void testTennisGame_EachPlayerWin4Points_Score_Deuce() throws TennisGameException {
 		//Arrange
 		TennisGame game = new TennisGame();
 		
@@ -44,7 +169,7 @@ public class TennisGameTest {
 		game.player1Scored();
 		game.player2Scored();
 		//Act
-		String score = game.getScore() ;
+		String score = game.getScore();
 		// Assert
 		assertEquals("Tie score incorrect", "deuce", score);		
 	}
@@ -61,5 +186,7 @@ public class TennisGameTest {
 		//Act
 		// This statement should cause an exception
 		game.player1Scored();			
-	}		
+	}
+	
+	// examples end.
 }
